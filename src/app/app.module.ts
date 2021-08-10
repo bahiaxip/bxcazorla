138d35b2a03d5bf7b3025e6421d943403919e644
Xip <bahiaxip@hotmail.com>
2021-08-10 09:17:04 +0200

    added sections and buttons sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GalleryModule } from 'ng-gallery';
+import { HomeComponent } from './home/home.component';
+
+//material
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+//import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    GalleryModule,
+    //material
+    MatButtonModule,
+    MatListModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    //CdkAccordionModule,
+    MatExpansionModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GalleryItem, ImageItem} from 'ng-gallery';
+
+import { MatAccordion } from '@angular/material/expansion';
+
+@Component({
+  selector: 'pre-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  
+  private selectedSection:any=null;
+  public images2:any;
+  panelOpenState=false;
+  images: any;
+  @ViewChild('section1',{static:true}) private section1!:ElementRef;
+  @ViewChild('section2',{static:true}) private section2!:ElementRef;
+  @ViewChild('section3',{static:true}) private section3!:ElementRef;
+  constructor() {
+    //window.addEventListener("")
+    //this.selectedSection=this.section1.nativeElement;
+  }
+
+  ngOnInit(): void {
+    this.selectedSection=this.section1.nativeElement;
+    //document.getElementsByTagName("html")[0].style.overflow="hidden";
+    window.addEventListener("resize",(e)=>{
+      this.selectedSection.scrollIntoView();
+      console.log("se esta moviendo:",e);
+      console.log("el scrolltop es: ",this.section2.nativeElement.scrollTop)
+    })
+    
+    //window.removeEventListener("scroll",disableScroll);
+
+    this.images=[
+      new ImageItem({
+        src:'./assets/river.jpg',thumb:'./assets/id_unity.jpeg'
+      }),
+      new ImageItem({
+        src:'./assets/rio_borosa.jpg',thumb:'./assets/id_unity.jpg'
+      }),
+      new ImageItem({
+        src:'./assets/rio_cazorla.jpg',thumb:'./assets/id_unity.jpg'
+      }),
+      new ImageItem({
+        src:'./assets/salto_organos.jpg',thumb:'./assets/id_unity.jpg'
+      }),
+
+    ];
+  }
+
+  enviar(id:number){
+    if(id==1){
+      this.selectedSection=this.section1.nativeElement;
+      console.log("al 1")
+      this.selectedSection.scrollIntoView({behavior:"smooth"});       
+    }else if(id==2){
+      this.selectedSection=this.section2.nativeElement;
+      console.log("al 2")
+
+      this.selectedSection.scrollIntoView({behavior:"smooth",block:"center"});  
+      //this.section1.nativeElement.style.display="none"
+      //this.section1.nativeElement.style.height="auto";
+      console.log(this.section2.nativeElement.getBoundingClientRect().top)
+      console.log(this.section2.nativeElement.scrollTop);
+    }else if(id==3){
+      this.selectedSection=this.section3.nativeElement;
+      console.log("al 2")
+      this.selectedSection.scrollIntoView({behavior:"smooth",block:"center"});  
+    }
+
+
+    
+  }
+
+}
