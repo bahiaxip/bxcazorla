1de53189d39acaa3eec2c6d5d3b145f16adfd223
Xip <bahiaxip@hotmail.com>
2023-04-13 23:26:38 +0200

    add greensock library and animation to arroyofrio component

--- src/app/home/info-arroyo-frio/info-arroyo-frio.component.ts
+++ src/app/home/info-arroyo-frio/info-arroyo-frio.component.ts
@@ -1,7 +1,8 @@
 //import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild,ElementRef } from '@angular/core';
 import { CardService } from '../../services/card.service';
-
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 @Component({
   selector: 'pre-info-arroyo-frio',
   templateUrl: './info-arroyo-frio.component.html',
@@ -13,6 +14,14 @@ export class InfoArroyoFrioComponent implements OnInit {
   public headerTitle:any='Más sobre ArroyoFrío';
   public dataHeader:any;
   public headerPanels:any;
+
+  @ViewChild('back_articles',{static:true}) private back_articles!:ElementRef;
+  @ViewChild('arroyofrio',{static:true}) private arroyofrio!:ElementRef;
+  @ViewChild('aventura',{static:true}) private aventura!:ElementRef;
+  @ViewChild('freeRoute',{static:true}) private freeRoute!:ElementRef;
+  @ViewChild('gastronomy',{static:true}) private gastronomy!:ElementRef;
+  @ViewChild('nature',{static:true}) private nature!:ElementRef;
+  
   constructor(private _cardService:CardService) { }
 
   ngOnInit(): void {
@@ -24,12 +33,73 @@ export class InfoArroyoFrioComponent implements OnInit {
       section:1,
       left : 1
     }
+    this.animationGsap();
+
   }
 
   sendPanel(panel:number){ 
     console.log("pasa x info-arroyofrio.component")   
     this._cardService.setPanel(panel);
     this._cardService.setSection(1)
+  }
+
+  animationGsap(){
+    gsap.registerPlugin(ScrollTrigger);
+    const back_articles = this.back_articles.nativeElement;
+    const arroyofrio = this.arroyofrio.nativeElement;
+    const adventure = this.aventura.nativeElement;
+    const freeRoute = this.freeRoute.nativeElement;
+    const gastronomy = this.gastronomy.nativeElement;
+    const nature = this.nature.nativeElement;
+    gsap.to(arroyofrio,{opacity:1})
+    gsap.to(adventure,{
+      opacity:1,
+      duration:2,
+      scrollTrigger:{
+        scroller: back_articles,
+        trigger:arroyofrio,
+        markers:false,
+        scrub:4,
+
+        start: '100 top',
+        end:'+=900 bottom'
+      }
+    });
+    gsap.to(freeRoute,{
+      opacity:1,
+      duration:2,
+      //x:0,
+      scrollTrigger:{
+        scroller: back_articles,
+        trigger:adventure,
+        markers:false,
+        scrub:1,
+        start: 'top top',
+        end:'+=1500 bottom'
+      }
+    });
+    gsap.to(gastronomy,{
+      opacity:1,
+      scrollTrigger:{
+        scroller:back_articles,
+        trigger:freeRoute,
+        markers:false,
+        scrub:1,
+        start:'200 top',
+        end:'+=900 bottom'
+      }
+    })
+    gsap.to(nature,{
+      opacity:1,
+      scrollTrigger:{
+        scroller:back_articles,
+        trigger:gastronomy,
+        markers:false,
+        scrub:1,
+        start:'top top',
+        end:'+=900 bottom'
+      }
+    })
   }
 
 }
